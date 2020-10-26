86f1c3cb8902f3042ec19cef898c19caa1f2343f
margauxln <64082890+margauxln@users.noreply.github.com>
2020-10-26 15:52:23 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,9 +1,14 @@
+// creation de la variable qui contient les images du site internet sous la forme d'un tableau
+
 var img = document.getElementsByTagName("img");
 console.log(img);
+
+// Chrome récupere la source de l'image choisie
 
 var pict = "images/kitten.jpg"
 var url = chrome.extension.getURL(pict);
 
+// boucle pour changer toutes les url des images du site par celle pré-définie
 for(var i = 0; i < img.length ; i++) {
     img[i].src = url;
   }
