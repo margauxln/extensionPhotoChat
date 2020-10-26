a1fb72d11d64db1ed3f7b2444af94c25bd396922
Margaux Lenne <margaux.lennebonety@gmail.com>
2020-10-26 14:18:57 +0100

    Extension Photo Chat

---
+++ content.js
@@ -0,0 +1,9 @@
+var img = document.getElementsByTagName("img");
+console.log(img);
+
+var pict = "images/kitten.jpg"
+var url = chrome.extension.getURL(pict);
+
+for(var i = 0; i < img.length ; i++) {
+    img[i].src = url;
+  }
---
+++ popup.js
@@ -0,0 +1,56 @@
+
+var btnKitten = document.getElementById("kitten");
+btnKitten.addEventListener('click', function () {
+console.log("kitten");
+})
+
+
+// var listOfImages = Array.from(document.getElementsByTagName("img").innerHTML);
+// console.log(listOfImages);
+
+// var btn = document.getElementById("save");
+// console.log("btn= "+ btn);
+//
+// function pictUrl (animalName){
+//   switch(animalName){
+//     case "Kitten":
+//       return browser.extension.getURL("images/kitten.jpg");
+//     case "Rabbit":
+//       return browser.extension.getURL("images/rabbit.jpg");
+//     case "Deer":
+//       return browser.extension.getURL("images/deer.jpg");
+//     case "Puppy":
+//         return browser.extension.getURL("images/puppy.jpg");
+//   }
+//
+// }
+//
+// btn.addEventListener('click', function () {
+//   let animalChosen = document.getElementsByTagName("img").innerHTML;
+//   let animalChosenUrl = pictUrl(animalChosen);
+// })
+//
+
+
+
+// }
+//
+//
+//
+// document.addEventListener("click", (e) => {
+//   if (e.target.classList.contains("img")) {
+//     var animalName = e.target.textContent;
+//     var animalNameURL = pictUrl(animalName);
+//
+//
+//
+//     var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+//     gettingActiveTab.then((tabs) => {
+//       browser.tabs.sendMessage(tabs[0].id, {animalUrl: animalNameURL});
+//     });
+//   }
+//   else if (e.target.classList.contains("clear")) {
+//     browser.tabs.reload();
+//     window.close();
+//   }
+// });
